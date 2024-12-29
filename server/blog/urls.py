e933c2da32ed27ff7ee6d5c834940ebde821904c
= <=>
2024-12-29 05:40:59 +0400

    change urls: Add url for delete_post

--- server/blog/urls.py
+++ server/blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('logout/', views.logoutt, name = 'logoutt'),
     path('register/', views.register, name = 'register'),
     path('blog/', views.home, name = 'blog_page'),
-    path('blog_post/', views.post_page, name = 'post_page')
+    path('blog_post/', views.post_page, name = 'post_page'),
+    path('delete_post/', views.delete_post, name = 'delete_post')
 ] 
