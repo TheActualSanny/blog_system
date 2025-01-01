0a26fcfe3acfa62b9af413fbad6dd5e7a1ef8f95
= <=>
2025-01-01 04:42:30 +0400

    change settings: Add static and media roots to save images

--- server/server/urls.py
+++ server/server/urls.py
@@ -17,6 +17,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from blog import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,4 @@ urlpatterns = [
     # path('accounts/register/', views.register, name = 'register'),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
